536e81fe6eafbde47dc9f729e9e62b52f5e29375
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2023-03-31 00:37:30 -0400

    init for playtest - iteration 2

--- Prototype 4B - Planets/Assets/Scripts/GravityBody.cs
+++ Prototype 4B - Planets/Assets/Scripts/GravityBody.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 [RequireComponent (typeof (Rigidbody))]
 public class GravityBody : MonoBehaviour {
 	
 	public GravityAttractor planet;
 	public Rigidbody rigidbody;
 	public GameObject lastplanet;
+	public GameObject variable_score;
+	public int score;
 	
 	void Awake () {
 		planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
-		
+		score = 0;
+		variable_score.GetComponent<TextMeshProUGUI>().text = score + "";
 		rigidbody = GetComponent<Rigidbody>();
-
+		
 		// Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
 		rigidbody.useGravity = false;
 		rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
@@ -33,14 +37,30 @@ public class GravityBody : MonoBehaviour {
 		// Allow this body to be influenced by planet's gravity
 		planet.Attract(rigidbody);
 	}
-	
+    private void OnTriggerEnter(Collider other)
+    {
+		if (other.CompareTag("alien") && gameObject.CompareTag("Player"))
+		{
+			other.gameObject.GetComponentInChildren<TextMeshPro>().enabled = true;
+			
+		}
+		if (other.CompareTag("pickup") && gameObject.CompareTag("Player"))
+		{
+			Destroy(other.gameObject);
+			score++;
+			variable_score.GetComponent<TextMeshProUGUI>().text = score + "";
+
+		}
+	}
+
     private void OnCollisionEnter(Collision collision)
     {
-		if (collision.collider.CompareTag("alien"))
+		/*if (collision.collider.CompareTag("alien"))
 		{
-			Debug.Log("Contact alien");
+			Debug.Log("OK");
+			collision.collider.gameObject.GetComponentInChildren<TextMesh>().gameObject.SetActive(true);
 		}
-
+		*/
 			if (collision.collider.CompareTag("Friendly Planet"))
 		{
 			lastplanet.GetComponent<AudioSource>().Pause();
@@ -53,6 +73,13 @@ public class GravityBody : MonoBehaviour {
 		{
 			var death = new death();
 			death.Die();
+		};
+		if (collision.collider.CompareTag("spaceship"))
+		{
+			if (score >= 4)
+            {
+				SceneManager.LoadScene("End");
+            }
 		};
 	}
 }
